cb4dff65a105ed3e09461b9c5ca2e501eac64798
aarav3g <88558768+aarav3g@users.noreply.github.com>
2021-08-08 12:54:41 -0400

    Update and rename Rocket Database.sql to Rocket Database.sql.js

--- Rocket Database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE RocketData (
-  Company varchar(200),
-  Model VARCHAR(200),
-  Height VARCHAR(200),
-  EngineNum int,
-  EngineName varchar(200)
-);
-
-
---
+++ Rocket Database.sql.js
@@ -0,0 +1,13 @@
+CREATE TABLE RocketData (
+  Company varchar(200),
+  Model varchar(200),
+  Height varchar(200),
+  EngineNum int,
+  EngineName varchar(200),
+  Launches int
+);
+
+INSERT INTO RocketData
+VALUES ("SpaceX", "Falcon 9", "70 m", 9, "Merlin", 122) 
+VALUES ("SpaceX", "Falcon Heavy", "70 m", 27, "Merlin", 3);
+
