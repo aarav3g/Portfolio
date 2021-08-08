6fdbe1a35950d3f81aacfaec469565f1004ad967
aarav3g <88558768+aarav3g@users.noreply.github.com>
2021-08-08 13:02:02 -0400

    Update Rocket Database.sql.js

--- Rocket Database.sql.js
+++ Rocket Database.sql.js
@@ -1,3 +1,4 @@
+/*Created Table*/
 CREATE TABLE RocketData (
   Company varchar(200),
   Model varchar(200),
@@ -7,7 +8,9 @@ CREATE TABLE RocketData (
   Launches int
 );
 
+/*Inserted data into table */
 INSERT INTO RocketData
-VALUES ("SpaceX", "Falcon 9", "70 m", 9, "Merlin", 122) 
-VALUES ("SpaceX", "Falcon Heavy", "70 m", 27, "Merlin", 3);
+VALUES ("SpaceX", "Falcon 9", "70 m", 10, "Merlin", 122) 
+VALUES ("SpaceX", "Falcon Heavy", "70 m", 28, "Merlin", 3)
+VALUES ("SpaceX", "Starship", "120 m", 35, "Raptor", 0);
 
