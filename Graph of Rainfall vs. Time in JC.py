682d005d1da05036ea678aab1b4268d374995538
aarav3g <88558768+aarav3g@users.noreply.github.com>
2022-01-03 16:54:26 -0500

    Create Graph of Rainfall vs. Time in JC.py

---
+++ Graph of Rainfall vs. Time in JC.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.rcParams.update({'figure.figsize':(9,5), 'figure.dpi':90})
+x = np.arange(0, 30, 1)
+y = (x * 1.0036) + 53.7
+
+
+
+plt.plot(x, y)
+plt.title("Rainfall vs. Time (years from 2020) in Johns Creek, Georgia \n (accounting for increase due to climate change)")
+plt.xlabel("Time (years from 2020)")
+plt.ylabel("Amount of rainfall (inches)")
+plt.show()
+
+
+avg_rainfall = float(input("Enter the average annual rainfall: "))
+predicted_avg_rainfall = (avg_rainfall * 1.0036)
+print ("The predicted average annual rainfall is " + str(predicted_avg_rainfall))
